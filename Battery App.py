12d631f9982dbe0ff7c391c1c867858cdeb2fafd
Varun <varun.kumar@xaviers.edu.in>
2022-07-02 17:46:07 +0530

    Added Basic TTS

--- Battery App.py
+++ Battery App.py
@@ -2,24 +2,32 @@ import tkinter as tk
 import psutil as ps
 
 
+def fetchbatterypercent():
+    Battery = ps.sensors_battery()
+    percent = Battery.percent
+    ChargingStatus = Battery.power_plugged
+    if ChargingStatus:
+        ChargingText = "Charging Battery"
+    else:
+        ChargingText = "Device Unplugged"
+    return percent, ChargingText
+
+
+percent, pluggedstatus = fetchbatterypercent()
+
+
+# GUI Design
 window = tk.Tk()
 window.title("Battery App")
 
 TitleText = tk.Message(text="Battery % Viewer")
 TitleText .pack()
 
-Battery = ps.sensors_battery()
-percent = Battery.percent
-ChargingStatus = Battery.power_plugged
-if ChargingStatus:
-    ChargingText = "Charging Battery"
-else:
-    ChargingText = "Device Unplugged"
 
 BatteryLevelText = tk.Message(text="Battery Level " + str(percent) + " %")
 BatteryLevelText.pack()
 
-ChargingStatusText = tk.Message(text=ChargingText)
+ChargingStatusText = tk.Message(text=pluggedstatus)
 ChargingStatusText.pack()
 
 window.mainloop()
